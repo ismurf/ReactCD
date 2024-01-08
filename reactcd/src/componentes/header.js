f1aadd57992393bcea79e3e1fc828fc8103b92bb
Milton <papasmurfsays@gmail.com>
2024-01-07 23:55:31 -0600

    se borró el header de app.js, en src se generó la carpeta componentes y dentro el archivo header.js

---
+++ reactcd/src/componentes/header.js
@@ -0,0 +1,24 @@
+function header(){
+    return(
+        <div className="banner">
+            <img src="" alt=""></img>
+            <div className="logo">
+                <img src="" alt=""></img>
+            </div>
+            <nav className="menu">
+                <ul className="upperMenu">
+                    <li><a href="">Servicios</a></li>
+                    <li><a href="">Quiénes Somos</a></li>
+                    <li><a href="">Contacto</a></li>
+                    <li><a href="">FAQ</a></li>
+                </ul>
+            </nav>
+
+            
+       </div>
+
+
+    )
+}
+
+export default header;
